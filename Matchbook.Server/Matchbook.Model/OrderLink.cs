3619ce04662ee5cc771ec53dc97d90fef2d2f6ab
mkbhanu <mkbhanuprakash@gmail.com>
2020-11-19 12:16:31 +0530

    Add files via upload

---
+++ Matchbook.Server/Matchbook.Model/OrderLink.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matchbook.Model
+{
+    public class OrderLink
+    {
+        public int Id { get; set; }
+
+        public ICollection<Order> LinkedOrders { get; set; }
+    }
+}
---
+++ Matchbook.Server/Matchbook.WebHost/Controllers/OrdersController.cs
@@ -0,0 +1,51 @@
+﻿using Matchbook.Db;
+using Matchbook.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Matchbook.WebHost.Controllers
+{
+    public class OrderSummary
+    {
+        public int Id { get; set; }
+        public string ProductSymbol { get; set; }
+        public decimal? Price { get; set; }
+        public string Currency { get; set; }
+        public int Quantity { get; set; }
+        public string UnitOfMeasure { get; set; }
+        public int SubAccount { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly MatchbookDbContext dbContext;
+
+        public OrdersController(MatchbookDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<OrderSummary>>> Get()
+        {
+            return await dbContext.Orders
+                .Select(o => new OrderSummary
+                {
+                    Id = o.Id,
+                    ProductSymbol = o.ProductSymbol,
+                    Price = o.Price,
+                    Currency = o.Product.Specification.PriceQuoteCurrency,
+                    Quantity = o.Quantity,
+                    UnitOfMeasure = o.Product.Specification.ContractUoM,
+                    SubAccount = o.SubAccountId
+                })
+                .ToListAsync();
+        }
+    }
+}
